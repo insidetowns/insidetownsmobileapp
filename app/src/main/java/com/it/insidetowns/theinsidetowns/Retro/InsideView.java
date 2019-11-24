185bb0127f6c61277dc108b07ead5d5a1b345fdd
sarma <shamgarphp@gmail.com>
2019-11-24 11:14:13 +0530

    Remove
    Signed-off-by: sarma <shamgarphp@gmail.com>

--- app/src/main/java/com/it/insidetowns/theinsidetowns/Retro/InsideView.java
+++
@@ -1,9 +0,0 @@
-package com.it.insidetowns.theinsidetowns.Retro;
-
-import retrofit2.Response;
-
-public interface InsideView {
-    void setSuccessResponse(Response response, int requestType);
-
-    void setErrorResponse(String message, int requestType);
-}
